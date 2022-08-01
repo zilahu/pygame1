484892297c838962fdb2946c78a9015dfa8d1101
Kókai László <kokai.laszlo@gmail.com>
2022-08-01 16:07:50 +0200

    code formatting

--- 2_drawing_on_display.py
+++ 2_drawing_on_display.py
@@ -2,21 +2,21 @@ import pygame
 
 pygame.init()
 
-WINDOW_WIDTH=600
-WINDOW_HEIGHT=600
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Drawing objects")
 
 running = True
 
-#Define colors
+# Define colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-YELLOW = (255, 255,0)
+YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 
@@ -24,24 +24,31 @@ MAGENTA = (255, 0, 255)
 display_surface.fill(BLUE)
 
 # draw lines
-pygame.draw.line(display_surface, RED, (0,0), (100,100), 5)
-pygame.draw.line(display_surface, GREEN, (100,100), (200,300), 1)
+pygame.draw.line(
+    display_surface, RED, (0, 0), (100, 100), 5
+)
+pygame.draw.line(
+    display_surface, GREEN, (100, 100), (200, 300), 1
+)
 
 # draw circles
-pygame.draw.circle(display_surface, WHITE, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 200, 6)
-pygame.draw.circle(display_surface, YELLOW, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 195, 0)
+pygame.draw.circle(
+    display_surface, WHITE, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 200, 6
+)
+pygame.draw.circle(
+    display_surface, YELLOW, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 195, 0
+)
 
 # draw rectangles
-pygame.draw.rect(display_surface, CYAN, (500, 0, 100, 100)) 
+pygame.draw.rect(display_surface, CYAN, (500, 0, 100, 100))
 pygame.draw.rect(display_surface, MAGENTA, (500, 100, 50, 100))
 
 # Game main loop
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-		# update display
-		pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        # update display
+        pygame.display.update()
 
 pygame.quit()
-
